893ca8f5bef90951f7b6f038343f8ae19459a802
Yaroslav <kovshun.yv@gmail.com>
2019-12-05 23:26:04 +0200

    Updates

---
+++ precache-manifest.20209fa02334e442806b6958670f6d46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5009b96f3371d585b21a163d1ce5c5",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "f818240f0dbc5139191e",
+    "url": "/goit-react-hw-02-reader/static/css/main.b310fb38.chunk.css"
+  },
+  {
+    "revision": "f88129380e42c4053525",
+    "url": "/goit-react-hw-02-reader/static/js/2.9b09be32.chunk.js"
+  },
+  {
+    "revision": "f818240f0dbc5139191e",
+    "url": "/goit-react-hw-02-reader/static/js/main.9a372670.chunk.js"
+  },
+  {
+    "revision": "299f48f5aa6019a15bd5",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.ef9e362e.js"
+  }
+]);
